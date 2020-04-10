a993ad29edbbc8921e195dc83410e32bb48761df
@douglasabnovato <douglasabnovato@gmail.com>
2020-04-10 00:30:00 -0300

    design da página Login

--- series-wished/src/modules/auth/pages/Login.vue
+++ series-wished/src/modules/auth/pages/Login.vue
@@ -1,13 +1,39 @@
 <template>
-    <div>
-        <h1>Login</h1>        
-    </div>
+    <form @submit.prevent="submit()">
+        <div class="login-page">
+            <div class="card">
+                <div class="card-header">
+                    Login
+                </div>
+                <div class="card-body">
+                    <div class="form-group">
+                        <input required type="email" v-model="formEmail" class="form-control" placeholder="E-mail">
+                    </div>
+                    <div class="form-group">
+                        <input required type="password" placeholder="senha" class="form-control" v-model="formPassword">
+                    </div>
+                    <button class="btn btn-primary w-100">
+                        Entrar
+                    </button>
+                </div>
+            </div>
+        </div>
+    </form>
 </template>
+
 <script>
     export default {
-        
+  
     }
 </script>
-<style lang="sass" scoped>
-
+<style scoped lang="scss">
+.login-page {
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .card {
+    width: 30%;
+  }
+}
 </style>
