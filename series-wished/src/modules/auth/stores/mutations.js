66bcae3510cfb740dd56ad500235389b581bea38
@douglasabnovato <douglasabnovato@gmail.com>
2020-04-15 19:28:22 -0300

    definição de mutations

--- series-wished/src/modules/auth/stores/mutations.js
+++ series-wished/src/modules/auth/stores/mutations.js
@@ -1,3 +1,10 @@
+import * as types from './mutations-types'
+
 export default {
-    
+    [types.SET_USER] (state, payload) {
+        state.user = payload
+    },
+    [types.TOKEN] (state, payload) {
+        state.token = payload
+    }     
 }
