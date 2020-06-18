97a4b81edbb4381ba1e767102846e9837bc2c2d9
@douglasabnovato <douglasabnovato@gmail.com>
2020-06-17 22:20:09 -0300

    interceptar o token

--- series-wished/src/http/interceptor.js
+++
@@ -1,7 +0,0 @@
-export default req => {
-    return ({ status }) => {
-        if( status === 401 ){
-            
-        }
-    }
-}
--- series-wished/src/main.js
+++ series-wished/src/main.js
@@ -1,16 +1,16 @@
 import Vue from "vue";
 import App from "./App";
-import router from "./router";
 import store from "./store";
-import VueResource from 'vue-resource'
+import router from "./router";
+//import VueResource from 'vue-resource'
 
 import './assets/scss/app.scss';
 
-Vue.use(VueResource);
+//Vue.use(VueResource);
 
 Vue.config.productionTip = false
 
-new Vue({
+window._Vue = new Vue({
   router,
   store,
   render: h => h(App)
